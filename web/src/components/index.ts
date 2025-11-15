02de19fdaa42f6af0037e596e5dd7cb2891258f5
Brijesh <softbrijeshio@gmail.com>
2025-11-15 12:19:40 +0530

    feat(web): add searchbar component

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./Navbar";
 export * from "./JobCard";
+export * from "./Searchbar";
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,11 +1,17 @@
-import { JobCard } from "@/components";
+import { JobCard, Searchbar } from "@/components";
 import { jobs } from "../../data/jobs";
 import type { JobCardProps } from "@/types";
 
 export function Home() {
   return (
-    <main className="my-3">
-      <section className="container">
+    <main className="container my-3">
+      {/* Searchbar */}
+      <section className="mb-2">
+        <Searchbar />
+      </section>
+
+      {/* Job Cards */}
+      <section>
         <h2 className="fw-bold">All Jobs</h2>
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
           {jobs.map((job: JobCardProps) => (
